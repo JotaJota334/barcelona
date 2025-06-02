bbc52562cefcd7bf8ee9ea700d46cc0933053d57
JotaJota334 <joaopedrodesouzza8@gmail.com>
2025-06-02 11:21:09 -0300

    Separação do CSS para visualizar os cards e da escalação do time

--- processamento/funcoesBD.php
+++ processamento/funcoesBD.php
@@ -14,6 +14,7 @@ function InserirCard($nome, $posicao, $imagem) {
 function RecuperarCard() {
     $conexao = conectarBD();
     $consulta = "SELECT nome, posicao, imagem FROM cards";
+    
     $resultado = mysqli_query($conexao, $consulta);
     
     $cards = array();
